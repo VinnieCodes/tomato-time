99c159d7806ecdd3832acc37223a5d47ff93a792
VinnieCodes <vincentathorne2005@gmail.com>
2025-06-25 15:38:01 -0700

    plus icon changed

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -9,14 +9,22 @@ function TaskForm({onAdd}) {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <button>+</button>
-        <input
-          type="text"
-          value={taskName}
-          onChange={(ev) => setTaskName(ev.target.value)}
-          placeholder="Your next task..."
-        />
-      
+      <button >
+        <svg style={{marginTop: "2px"}}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="size-5"
+        >
+          <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+        </svg>
+      </button>
+      <input
+        type="text"
+        value={taskName}
+        onChange={(ev) => setTaskName(ev.target.value)}
+        placeholder="Your next task..."
+      />
     </form>
   );
 }
